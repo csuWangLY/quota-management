40264b6476fbe5e1b38bd9d0500f4100d8c6239b
merci <1311747896@qq.com>
2024-04-27 15:54:01 +0800

    init commit

---
+++ src/main/java/com/project/quotamanagement/quotamanagement/QuotamanagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.quotamanagement.quotamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotamanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotamanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/quotamanagement/quotamanagement/QuotamanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quotamanagement.quotamanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotamanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
